06983094a48c45bd910c8aa29fdc6a1898df50cb
Shashank Rai <shashankrai0805@gmail.com>
2025-07-18 10:57:05 +0530

    Prepare for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()],
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+  }
 })
